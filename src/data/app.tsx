916345d6c0296b18855f64a61f00abbbf55bfbc3
Nuryan Yasmim <nuryanyasmim@gmail.com>
2026-06-14 08:25:42 -0300

    Create App component with Dashboard import

---
+++ src/data/app.tsx
@@ -0,0 +1,13 @@
+import Dashboard from "./compomentes/Dashboard";
+
+function App() {
+  return (
+    <div>
+      <h1>TeamFlow</h1>
+
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
